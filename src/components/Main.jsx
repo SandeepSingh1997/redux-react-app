f7a78de4c70651a1be3c017fee3175328d110269
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-06-16 11:24:42 +0530

    added compoenents

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+
+function Main() {
+  return (
+    <main>
+      <AddTodo />
+      <TodoList />
+    </main>
+  );
+}
+
+export default Main;
